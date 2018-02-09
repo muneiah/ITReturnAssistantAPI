6624eebc98ee0c97244f0393f88d9e859bc3e031
Rajat Thareja <rajat.thareja.1990@gmail.com>
2018-02-09 14:11:36 +0530

    change url

--- src/main/java/ITReturnAssistant/ITReturnAssistantAPI/ApplicationController.java
+++ src/main/java/ITReturnAssistant/ITReturnAssistantAPI/ApplicationController.java
@@ -16,17 +16,17 @@ public class ApplicationController {
 
     private MongoOperations db = (MongoOperations) new AnnotationConfigApplicationContext(MongoConfig.class).getBean("mongoTemplate");
 
-    @GetMapping("/admin/getYears")
+    @GetMapping("/getYears")
     public List<FinancialYear> getYears() {
         return db.find(new Query(), FinancialYear.class);
     }
 
-    @GetMapping("/admin/getUser/{id}")
+    @GetMapping("/getUser/{id}")
     public User getUser(@RequestParam(value="id") String id, @RequestBody String password) {
         return db.findOne(new Query(Criteria.where("id").is(id).and("password").is(password)), User.class);
     }
 
-    @PostMapping("/admin/saveUser")
+    @PostMapping("/saveUser")
     public boolean getUser(@RequestBody User user) {
         db.save(user);
         return true;
