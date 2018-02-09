d5737feb129b2e9ab16d6316bbd391bc4b3e3360
Rajat Thareja <rajat.thareja.1990@gmail.com>
2018-02-09 12:23:07 +0530

    first api

--- src/main/java/ITReturnAssistant/ITReturnAssistantAPI/ApplicationController.java
+++ src/main/java/ITReturnAssistant/ITReturnAssistantAPI/ApplicationController.java
@@ -1,14 +1,23 @@
 package ITReturnAssistant.ITReturnAssistantAPI;
 
 import ITReturnAssistant.ITReturnAssistantAPI.config.MongoConfig;
+import ITReturnAssistant.ITReturnAssistantAPI.modal.FinancialYear;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class ApplicationController {
 
     private MongoOperations db = (MongoOperations) new AnnotationConfigApplicationContext(MongoConfig.class).getBean("mongoTemplate");
 
+    @GetMapping("/admin/getYears")
+    public List<FinancialYear> getYears() {
+        return db.find(new Query(), FinancialYear.class);
+    }
 
 }
---
+++ src/main/java/ITReturnAssistant/ITReturnAssistantAPI/modal/FinancialYear.java
@@ -0,0 +1,22 @@
+package ITReturnAssistant.ITReturnAssistantAPI.modal;
+
+public class FinancialYear {
+    private String id;
+    private boolean isActive;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean active) {
+        isActive = active;
+    }
+}
